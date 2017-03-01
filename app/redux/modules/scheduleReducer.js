e64d69f5386105b8d2cbbfcf2d42178f1a3420c5
Scott Fuoco <scottfuoco@Scotts-MacBook-Pro.local>
2017-03-01 11:11:05 -0800

    initial commit

---
+++ app/redux/modules/scheduleReducer.js
@@ -0,0 +1,20 @@
+import {formatSessionData} from '../../helpers/dataFormatHelpers'
+
+const initialState = {
+  sessionData: {
+    dataBlob: {},
+    sectionIds: [],
+    rowIds: [],
+  }
+}
+
+const scheduleReducer = (state = initialState, action) => {
+  switch (action.type) {
+    case 'LOAD_SCHEDULE':
+      return { sessionData: formatSessionData(action.payload) };
+    default:
+      return state;
+  }
+};
+
+export default scheduleReducer;
