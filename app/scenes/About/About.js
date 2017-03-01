eb4c24c0ccf6331220b78cf6cc12f3a7a7730845
Scott Fuoco <scottfuoco@Scotts-MacBook-Pro.local>
2017-03-01 11:10:59 -0800

    renamed variables

--- app/scenes/About/About.js
+++ app/scenes/About/About.js
@@ -16,7 +16,7 @@ import Conduct from '../../components/Conduct';
 
 import styles from './styles'
 
-export default About = ({isLoading, dataSource}) => (
+export default About = ({isLoading, conducts}) => (
   <ScrollView style={styles.sceneContainer}>
     <View style={styles.logoContainer}>
       <Image
@@ -27,6 +27,6 @@ export default About = ({isLoading, dataSource}) => (
     <Text style={styles.description}>R10 is a conference that focuses on just about any topic related to dev.</Text>
     <Text style={styles.title}>Date & Venue</Text>
     <Text style={styles.description}>The R10 conference will take place on Tuesday, June 27 2017 in Vancouver, BC.</Text>
-    {dataSource.map(data => <Conduct key={data.title} title={data.title} description={data.description} />)}
+    {conducts.map(data => <Conduct key={data.title} title={data.title} description={data.description} />)}
   </ScrollView>
 )
