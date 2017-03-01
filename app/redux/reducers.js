84b2b9a1626bc3804eb729cf7c8a217d2604cf95
Scott Fuoco <scottfuoco@Scotts-MacBook-Pro.local>
2017-03-01 11:11:20 -0800

    added reducers for conducts schedule and loading

--- app/redux/reducers.js
+++ app/redux/reducers.js
@@ -1,7 +1,13 @@
 import { combineReducers } from 'redux';
 
 import { NavigationReducer } from '@exponent/ex-navigation';
+import conductsReducer from './modules/conductsReducer';
+import scheduleReducer from './modules/scheduleReducer';
+import isLoadingReducer from './modules/isLoadingReducer';
 
 export default combineReducers({
   navigation: NavigationReducer,
+  conducts: conductsReducer,
+  schedule: scheduleReducer,
+  isLoading: isLoadingReducer,
 });
