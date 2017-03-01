9bbcdcc55d65f3d99fb141255ae60b7995d7cd92
Scott Fuoco <scottfuoco@Scotts-MacBook-Pro.local>
2017-03-01 11:08:43 -0800

    style variables for colors and typography

---
+++ app/config/styles.js
@@ -0,0 +1,14 @@
+export const colors = {
+  black: '#000',
+  white: '#fff',
+  mediumGrey: '#999',
+  lightGrey: '#e6e6e6',
+  blue: '#8797D6',
+  purple: '#9963ea',
+  red: '#cf392a',
+}
+
+export const typography = {
+  fontMain: 'Montserrat',
+  fontMainLight: 'Montserrat-Light'
+}
