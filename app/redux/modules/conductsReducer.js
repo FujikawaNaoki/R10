bae2ad8938ce26b8622481bd264b0dad2dc2ca02
Scott Fuoco <scottfuoco@Scotts-MacBook-Pro.local>
2017-03-01 11:08:14 -0800

    created conductsReducer

---
+++ app/redux/modules/conductsReducer.js
@@ -0,0 +1,10 @@
+const conductsReducer = (state=[], action) => {
+  switch (action.type) {
+    case 'LOAD_CONDUCTS':
+      return action.payload;
+    default:
+      return state;
+  }
+};
+
+export default conductsReducer;
