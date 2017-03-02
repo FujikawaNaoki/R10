ff871313fedd5b970faac1a34475eb12dfac2c78
Scott Fuoco <scottfuoco@Scotts-MBP.hitronhub.home>
2017-03-01 19:05:37 -0800

    exported allFaves, createFave, removeFace helpers

--- app/config/models.js
+++ app/config/models.js
@@ -7,5 +7,15 @@ const Fave = {
     faved_on: 'date'
   }
 }
-export default new Realm({schema: [Fave]})
+export default new Realm({ schema: [Fave] })
 
+export const allFaves = realm => realm.objects('Fave').map(fave => fave.id)
+
+export const createFave = (realm, id) => {
+  realm.write(() => realm.create('Fave', { id, faved_on: new Date() }))
+}
+
+export const removeFave = (realm, id) => {
+  const FaveToRemove = realm.filtered('id == $0', id)
+  realm.write(() => realm.delete(FaveToRemove))
+}
