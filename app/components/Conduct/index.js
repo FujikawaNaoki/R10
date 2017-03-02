8164501f0441e9fc43202a01932ec84ece3eac6e
Scott Fuoco <scottfuoco@Scotts-MacBook-Pro.local>
2017-03-02 11:56:57 -0800

    added show/hide on conduct description animations

--- app/components/Conduct/index.js
+++ app/components/Conduct/index.js
@@ -1,14 +1,48 @@
-import React from 'react'
+import React, { Component } from 'react'
 import {
   Text,
+  LayoutAnimation,
+  TouchableOpacity,
   View
 } from 'react-native'
 
 import styles from './styles'
 
-export default ({title, description}) => (
-  <View>
-    <Text style={styles.title}>+ {title}</Text>
-    <Text style={styles.description}>{description}</Text>
-  </View>
-)
+class Conduct extends Component {
+
+  constructor() {
+    super();
+
+    this.state = {
+      descriptionVisible: false,
+    }
+  }
+
+  onPress = () => {
+    const config = LayoutAnimation.create(400, 'linear', 'opacity');
+    LayoutAnimation.configureNext(config);
+    this.setState({ descriptionVisible: !this.state.descriptionVisible })
+  }
+
+  descriptionVisibilitySymbol = () => this.state.descriptionVisible ? '-' : '+';
+
+  render() {
+    const { title, description } = this.props;
+    return (
+      <View>
+        <TouchableOpacity onPress={this.onPress}>
+          <Text style={styles.title}>
+            {this.descriptionVisibilitySymbol}
+            {title}
+          </Text>
+        </TouchableOpacity>
+        {
+          this.state.descriptionVisible &&
+          <Text style={styles.description}>{description}</Text>
+        }
+      </View>
+    )
+  }
+}
+
+export default Conduct
