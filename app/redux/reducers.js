ed56c9f9f2c85c68d1ac1c5ef8d2a58fa928a5e3
Scott Fuoco <scottfuoco@Scotts-MBP.hitronhub.home>
2017-03-01 19:05:12 -0800

    added speaker reducer

--- app/redux/reducers.js
+++ app/redux/reducers.js
@@ -3,11 +3,13 @@ import { combineReducers } from 'redux';
 import { NavigationReducer } from '@exponent/ex-navigation';
 import conductsReducer from './modules/conductsReducer';
 import scheduleReducer from './modules/scheduleReducer';
+import speakerReducer from './modules/speakerReducer';
 import isLoadingReducer from './modules/isLoadingReducer';
 
 export default combineReducers({
   navigation: NavigationReducer,
   conducts: conductsReducer,
   schedule: scheduleReducer,
+  speaker: speakerReducer,
   isLoading: isLoadingReducer,
 });
