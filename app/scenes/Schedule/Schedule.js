02b7c4cf1106ad1386f23590fd6175dc48515ba5
Scott Fuoco <scottfuoco@Scotts-MBP.hitronhub.home>
2017-03-04 12:38:29 -0800

    added heart icon to faved sessions

--- app/scenes/Schedule/Schedule.js
+++ app/scenes/Schedule/Schedule.js
@@ -11,16 +11,28 @@ import { formatSecondsToTime } from '../../helpers/timeFormatHelpers'
 import { goToSession } from '../../helpers/navigationHelpers'
 
 import styles from './styles'
+import { colors, platformIcons } from '../../config/styles'
+import Icon from 'react-native-vector-icons/Ionicons'
 
-export default Schedule = ({isLoading, schedule}) => (
+const Schedule = ({ schedule, goToSessionHistory, faves }) => (
   <View style={styles.container}>
     <ListView
       dataSource={schedule}
       renderRow={data => (
-        <TouchableHighlight onPress={() => goToSession('schedule', data)}>
+        <TouchableHighlight onPress={() => goToSession(goToSessionHistory, {...data, tab:goToSessionHistory} )}>
           <View style={styles.row}>
             <Text style={styles.title}>{data.title}</Text>
-            <Text style={styles.location}>{data.location}</Text>
+            <View style={styles.faveContainer}>
+              <Text style={styles.location}>{data.location}</Text>
+              {
+                faves.includes(data.session_id) &&
+                <Icon
+                  name={platformIcons.heart}
+                  size={25}
+                  color={colors.red}
+                />
+              }
+            </View>
           </View>
         </TouchableHighlight>
       )}
@@ -34,3 +46,5 @@ export default Schedule = ({isLoading, schedule}) => (
     />
   </View>
 )
+
+export default Schedule;
