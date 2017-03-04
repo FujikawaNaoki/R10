4f63680d37595159420321d702902216cb86c4d2
Scott Fuoco <scottfuoco@Scotts-MBP.hitronhub.home>
2017-03-04 12:33:51 -0800

    added favesSpeakerData needed for when tabbing between faves and schedule

--- app/redux/modules/speakerReducer.js
+++ app/redux/modules/speakerReducer.js
@@ -8,13 +8,29 @@ const initialState = {
     session: '',
     speaker_id: '',
     url: '',
+  },
+  faveSpeakerData: {
+    bio: '',
+    image: 'https://facebook.github.io/react/img/logo_og.png',
+    name: '',
+    session: '',
+    speaker_id: '',
+    url: '',
   }
 }
 
 const speakerReducer = (state = initialState, action) => {
   switch (action.type) {
     case 'LOAD_SPEAKER':
-      return { speakerData: formatDataObject(action.payload) }
+      return {
+        ...state,
+        speakerData: formatDataObject(action.payload)
+      }
+    case 'LOAD_FAVE_SPEAKER':
+      return {
+        ...state,
+        faveSpeakerData: formatDataObject(action.payload)
+      }
     default:
       return state
   }
