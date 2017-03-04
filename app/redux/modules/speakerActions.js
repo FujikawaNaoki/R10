faf31d3fecaea1573d295c75ccfd1d9989c358e0
Scott Fuoco <scottfuoco@Scotts-MBP.hitronhub.home>
2017-03-04 12:33:06 -0800

    loaded speaker data

--- app/redux/modules/speakerActions.js
+++ app/redux/modules/speakerActions.js
@@ -5,14 +5,26 @@ export const loadSpeaker = speaker => ({
   payload: speaker
 })
 
-export const fetchSpeaker = speakerId => dispatch => {
+export const loadFaveSpeaker = speaker => ({
+  type: 'LOAD_FAVE_SPEAKER',
+  payload: speaker
+})
+
+export const fetchSpeaker = (speakerId, tab) => dispatch => {
   const endpoint = `https://r10app-95fea.firebaseio.com/speakers.json?orderBy=%22speaker_id%22&equalTo=%22${speakerId}%22`
 
   dispatch(loadingResource())
   fetch(endpoint)
     .then(response => response.json())
     .then(json => {
-      dispatch(loadSpeaker(json))
+      switch (tab) {
+        case 'Faves':
+          dispatch(loadFaveSpeaker(json))
+          break
+        default:
+          dispatch(loadSpeaker(json))
+          break
+      }
       dispatch(doneLoading())
     })
     .catch(error => console.log(`Error fetching JSON: ${error}`))
