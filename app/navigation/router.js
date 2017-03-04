caa642e91586d3623710c55feecde2578d1c40d1
Scott Fuoco <scottfuoco@Scotts-MBP.hitronhub.home>
2017-03-04 12:10:20 -0800

    added faves, speaker, and maps scenes

--- app/navigation/router.js
+++ app/navigation/router.js
@@ -3,11 +3,17 @@ import { createRouter } from '@exponent/ex-navigation';
 import About from '../scenes/About';
 import Schedule from '../scenes/Schedule';
 import Session from '../scenes/Session';
+import Faves from '../scenes/Faves';
 import Layout from './NavigationLayout';
+import Speaker from '../scenes/Speaker';
+import Maps from '../scenes/Maps';
 
 export default Router = createRouter(() => ({
   layout: () => Layout,
   about: () => About,
   schedule: () => Schedule,
   session: () => Session,
+  faves: () => Faves,
+  speaker: () => Speaker,
+  maps: () => Maps,
 }));
