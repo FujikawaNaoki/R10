b646d70d1a1d2bc7980a3a0c3ca290e46cac023a
Scott Fuoco <scottfuoco@Scotts-MBP.hitronhub.home>
2017-03-04 12:07:31 -0800

    centered loading indicator

--- app/components/Loading/index.js
+++ app/components/Loading/index.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import { ActivityIndicator } from 'react-native'
 
-// import styles from './styles'
-export default ({title, description}) => (
-  <ActivityIndicator animating={true} size="small" color="black" />
-)
+import styles from './styles'
+
+export default () => <ActivityIndicator style={styles.activityIndicator} animating={true} size="small" color="black" />
+
